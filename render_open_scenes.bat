dfcca1ba9f296ad727e0d37bbf977641bf407073
Gfans <niexiao2008@163.com>
2018-09-01 21:04:11 +0800

    Output .png instead of .exr file on windows

--- render_open_scenes.bat
+++ render_open_scenes.bat
@@ -1,15 +1,6 @@
 SET sceneName=buddha
 
 REM Run pbrt
-bin\Release\pbrt.exe --nthreads 8 --outfile results\pbrt.exr scenes\simple\%sceneName%.pbrt
-
-REM Conver exr image to tiff image
-bin\Release\exrtotiff.exe %sceneName%.exr results\%sceneName%.tiff
-
-REM Display tiff image
-results\%sceneName%.tiff
-
-REM delete newly generated .exr files
-del %sceneName%.exr
+releaseBuild\Release\pbrt.exe --nthreads 8 --outfile results\pbrt.png ..\pbrt-v3-scenes\simple\%sceneName%.pbrt
 
 
