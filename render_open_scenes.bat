c8818dca41b644d9bb94d13812455fe38a951599
Gfans <niexiao2008@163.com>
2018-09-01 21:02:26 +0800

    Revert "Output .png instead of .exr file on windows"
    This reverts commit 05fad6c1225c5346ae8d9f069351d31cda4b5f3b.

--- render_open_scenes.bat
+++ render_open_scenes.bat
@@ -1,6 +1,15 @@
 SET sceneName=buddha
 
 REM Run pbrt
-releaseBuild\Release\pbrt.exe --nthreads 8 --outfile results\pbrt.png ..\pbrt-v3-scenes\simple\%sceneName%.pbrt
+bin\Release\pbrt.exe --nthreads 8 --outfile results\pbrt.exr scenes\simple\%sceneName%.pbrt
+
+REM Conver exr image to tiff image
+bin\Release\exrtotiff.exe %sceneName%.exr results\%sceneName%.tiff
+
+REM Display tiff image
+results\%sceneName%.tiff
+
+REM delete newly generated .exr files
+del %sceneName%.exr
 
 
